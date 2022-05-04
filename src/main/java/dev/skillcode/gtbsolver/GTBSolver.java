4e15682d37d20049404c9e753c40278062bcab92
SkillCode <david@hniessen.de>
2022-05-04 16:29:15 +0200

    Update version

--- src/main/java/dev/skillcode/gtbsolver/GTBSolver.java
+++ src/main/java/dev/skillcode/gtbsolver/GTBSolver.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 public class GTBSolver {
 
     public static final String MODID = "GTB";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String PREFIX = "§6§lGTBSolver §8» ";
     public static final String AUTHOR = "SkillCode [https://skillcode.dev]";
 
